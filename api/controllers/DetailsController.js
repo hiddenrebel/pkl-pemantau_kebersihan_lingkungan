a65bd1d2fb86474ba84a0b1ecb979fa06e7e24c0
Mira Pravitasari <mira.pravitasari@kudo.co.id>
2016-08-27 22:15:42 +0700

    location and save all detail

--- api/controllers/DetailsController.js
+++ api/controllers/DetailsController.js
@@ -6,11 +6,47 @@
  */
 
 module.exports = {
-	hi: function (req, res) {
-	    return res.send('Hi there!');
-	},
-  	bye: function (req, res) {
-	    return res.redirect('http://www.sayonara.com');
+	saveDetails: function (req, res) {
+		var params = req.params.all()
+		
+		// Location details
+		var loc = params.location
+		Location.create({time:loc.time,
+						latitude:loc.latitude,
+						longitude:loc.longitude,
+						speed:loc.speed}).exec(function createCB(err,created){});
+		// Sensor details
+		var arr = params.sensor
+		var len=arr.length;
+		for(var i=0; i<len; i++) {
+			var sensorInfo = arr[i]
+			switch(sensorInfo.sensor) {
+				case "humidity":
+					Humidity.create({
+						sensor:sensorInfo.sensor,
+						value:sensorInfo.value,
+						satuan:sensorInfo.satuan}).exec(function createCB(err,created){});
+						break;
+				case "temperature":
+					Temperature.create({
+						sensor:sensorInfo.sensor,
+						value:sensorInfo.value,
+						satuan:sensorInfo.satuan}).exec(function createCB(err,created){});
+						break;
+				case "Colevel":
+					Colevel.create({
+						sensor:sensorInfo.sensor,
+						value:sensorInfo.value,
+						satuan:sensorInfo.satuan}).exec(function createCB(err,created){});
+					break;
+				case "airquality":
+					Airquality.create({
+						sensor:sensorInfo.sensor,
+						value:sensorInfo.value,
+						satuan:sensorInfo.satuan}).exec(function createCB(err,created){});
+			}
+		}
+		return res.json('200',{message:sensorInfo.sensor+' info collected and stored '});
 	}
 };
 
